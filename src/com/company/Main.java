ff4c62dcf32978b4fd9bd082a0648f75c7fc6f0d
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-07-08 17:17:38 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int array1[] = {2,4,5,7,9};
+
+        System.out.println(array1[1]);
+
+    }
+}
